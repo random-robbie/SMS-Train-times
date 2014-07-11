0992767b3d136843f3c5af9893b2cc7da879338a
Robbie <robbie@xcellweb.co.uk>
2014-07-11 15:47:09 +0100

    Updates
    Major Updates and Update TextLocal API and Made a few More Variables.

--- config.php
+++ config.php
@@ -4,14 +4,26 @@
 $dbtype = "mysql";
 $dbhost = "localhost";
 $dbname = "trains";
+$dbtable = "Trains";
 $dbuser  = "MYSQL USERNAME";
 $dbpass  = "MYSQL PASSWORD";
-$smskey = "YOUR KEYWORD";  // NOTE YOU MUST ADD A SPACE FOR THIS TO WORK
 $station = ""; //Enter your local station name
 
 // database connection
 $dbh = new PDO("mysql:host=$dbhost;dbname=$dbname",$dbuser,$dbpass);
 
-//Train Station of Departure
-$station = "Hoylake";
+// Google Drive CSV for train times URL
+// SEE GIT HUB INSTRUCTIONS TO ALTER THIS CSV TO YOUR LOCAL STATION
+$url  = 'https://docs.google.com/spreadsheet/pub?key=0AuC2xjBKqlYadDJ2TzJkdFh5Wi03Wlh5NmMtdDNOdHc&single=true&gid=0&output=csv';
+
+//temp folder for the CSV
+$tmpcsv = "/tmp/trains.csv";
+
+// Textlocal account details
+$username = urlencode('youremail@address.com');
+$hash = urlencode('Your API hash');
+$senderid = "MyTrainTime"; //Who the messsage to appear from
+// 0 for test mode off will use credits and 1 for test mode on will get result from api but will not send.
+$test = "0"; 
+
 ?>
--- functions.php
+++ functions.php
@@ -5,27 +5,25 @@ include ('config.php');
 function sms ($sender,$message)
 {
 
-                // Authorisation details.
-                $uname = "YOUR TEXT LOCAL USERNAME";
-                $hash = "YOUR TEXT LOCAL HASH";
-
-                // Configuration variables. Consult http://www.txtlocal.co.uk/developers/ for more info.
-                $info = "1";
-                $test = "0";
-
-                // Data for text message. This is the text message data.
-                $from = "Trains"; // This is who the message appears to be from.
-                // 612 chars or less
-                // A single number or a comma-seperated list of numbers
-                $message = urlencode($message);
-                $data =
-                "uname=".$uname."&hash=".$hash."&message=".$message."&from=".$from."&selectednums=".$sender."&info=".$info."&test=".$test;
-                $ch = curl_init('http://www.txtlocal.com/sendsmspost.php');
-                curl_setopt($ch, CURLOPT_POST, true);
-                curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                $result = curl_exec($ch); // This is the result from the API
-                curl_close($ch);
+                
+	GLOBAL $username;
+	GLOBAL $hash;
+	GLOBAL $senderid;
+	GLOBAL $test;
+	// Message details
+	$numbers = urlencode($sender);
+	$sender2 = urlencode($senderid);
+	$message2 = urlencode($message);
+ 
+	// Prepare data for POST request
+	$data = 'username=' . $username . '&hash=' . $hash . '&test=' . $test .'&numbers=' . $numbers . "&sender=" . $sender2 . "&message=" . $message2 ;
+ 
+	// Send the GET request with cURL
+	$ch = curl_init('http://api.txtlocal.com/send/?' . $data);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	$response = curl_exec($ch);
+	curl_close($ch);
+	echo "<br><br>".$response."";
 
 }
 
@@ -38,9 +36,9 @@ function debug ($message)
 						
 						
 function downloaddue()
-{					// SEE GIT HUB INSTRUCTIONS TO ALTER THIS CSV TO YOUR LOCAL STATION
-					$url  = 'https://docs.google.com/spreadsheet/pub?key=0AuC2xjBKqlYadDJ2TzJkdFh5Wi03Wlh5NmMtdDNOdHc&single=true&gid=0&output=csv';
-					$path = '/tmp/trains.csv';
+{					GLOBAL $url;
+					GLOBAL $tmpcsv;
+
  
 					$download = curl_init($url);
 					curl_setopt($download, CURLOPT_RETURNTRANSFER, true);
@@ -49,7 +47,7 @@ function downloaddue()
  
 					curl_close($download);
  
-					file_put_contents($path, $data);
+					file_put_contents($tmpcsv, $data);
 						
 					}
 					
@@ -57,14 +55,17 @@ function downloaddue()
 function emptytable() 
 {
 				GLOBAL $dbh;
-				$empty = $dbh->prepare('TRUNCATE TABLE  `Trains`');
+				GLOBAL $dbtable;
+				$empty = $dbh->prepare('TRUNCATE TABLE  ".$dbtable."');
 				$empty->execute();
 				}
 				
 function importdue()
 {
 				GLOBAL $dbh;
-				$importduetimes = $dbh->prepare("LOAD DATA INFILE '/tmp/trains.csv' INTO TABLE `Trains` FIELDS TERMINATED BY ',' IGNORE 1 LINES;");
+				GLOBAL $dbtable;
+				GLOBAL $tmpcsv;
+				$importduetimes = $dbh->prepare("LOAD DATA INFILE '".$tmpcsv."' INTO TABLE ".$dbtable." FIELDS TERMINATED BY ',' IGNORE 1 LINES;");
 				$importduetimes->execute();
  
     		
@@ -74,7 +75,8 @@ function importdue()
 function oldtrains()
 {
 		GLOBAL $dbh;
-        $oldtrains = $dbh->prepare('DELETE  FROM `Trains` WHERE `due` < CURRENT_TIME');
+		GLOBAL $dbtable;
+        $oldtrains = $dbh->prepare('DELETE  FROM ".$dbtable." WHERE `due` < CURRENT_TIME');
 		$oldtrains->execute();
 						}
 						
@@ -82,7 +84,9 @@ function oldtrains()
 function nexttrain()
 {
 		GLOBAL $dbh;
-        $nexttrain = $dbh->prepare("SELECT * FROM `Trains` WHERE `due` > CURRENT_TIME LIMIT 0 , 1 ");
+		GLOBAL $station;
+		GLOBAL $dbtable;
+        $nexttrain = $dbh->prepare("SELECT * FROM ".$dbtable." WHERE `due` > CURRENT_TIME LIMIT 0 , 1 ");
 		$nexttrain->execute();
 		$result = $nexttrain->fetch(PDO::FETCH_ASSOC);
 		$iflate = $result['Status'];
@@ -96,7 +100,7 @@ function nexttrain()
 		$now = date('h:i');
 		$d = (strtotime($now) - strtotime($due))/60;
 		GLOBAL $message;
-		$message = ("The next train to depart from ".$hoylake." is ".$due." and is currently ".$status."");
+		$message = ("The next train to depart from ".$station." is ".$due." and is currently ".$status."");
 		echo $message;
 		
 						}
